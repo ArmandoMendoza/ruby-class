7e27cefb8727b71a659dfc5e4a243b600deffbe2
Armando Mendoza <fcoarmandomendoza@gmail.com>
2020-06-15 21:07:29 -0400

    Clase 15-6-2020

--- clases.rb
+++ clases.rb
@@ -10,25 +10,49 @@ class Model
     puts "No se encuentra el atributo"
   end
 
-  def save
-    "guardando en BD #{@name}, #{@age}"
-  end
-
   def self.all
-    records = []
-    BD.each do |data|
-      records.push new(data[:name], data[:age])
-    end
-    records 
+    file = self.to_s.downcase
+    OpenFile.new("#{file}.csv").fetch_data
+  rescue Errno::ENOENT
+    puts "No se ha encontrado archivo #{file}.csv"
   end
 
-  def self.find(name)
-    records = []
-    array = BD.select {|data| data[:name] == name }
-    array.each do |data|
-      records.push new(data[:name], data[:age])
+end
+
+class OpenFile
+  attr_accessor :data, :file
+
+  def initialize(file)
+    @file = file
+    @data = []
+  end
+
+  def fetch_data
+    n = 0
+    headers = []
+    File.foreach(file) do |line| 
+      if n == 0 
+        headers = line.split(",")
+      else
+        values = line.split(",")
+        set_data(headers, values)
+      end
+      n += 1
     end
-    records
+    @data
+  end
+
+  def add_data(hsh)
+  end
+
+  private
+
+  def set_data(headers, values)
+    hsh = { }
+    headers.each_with_index do |key, index|
+      hsh[key.chomp.to_sym] = values[index].chomp
+    end
+    @data << hsh    
   end
 
 end
@@ -41,18 +65,23 @@ class Car < Model
   attr_accessor :doors, :year
 end
 
+class Person < Model
+end
 
+of = OpenFile.new("user.csv")
+of.add_data({name: "Mario", age: 50})
 
+user = User.new({name: "Mario", age: 50})
+user.save
 
-
-BD = [
-  { name: "Armando", age: 40 },
-  { name: "Rodrigo", age: 30 },
-  { name: "Flor",    age: 42 },
-  { name: "Lucia",   age: 15 },
-  { name: "Rafael",  age: 33 },
-  { name: "Javier",  age: 43 }
-]
+# BD = [
+#   { name: "Armando", age: 40 },
+#   { name: "Rodrigo", age: 30 },
+#   { name: "Flor",    age: 42 },
+#   { name: "Lucia",   age: 15 },
+#   { name: "Rafael",  age: 33 },
+#   { name: "Javier",  age: 43 }
+# ]
 
 # TODO: hacer que find pueda buscar de la siguiente manera
 # User.find({nombre: "Armando"}) o User.find({age: 40}) 
